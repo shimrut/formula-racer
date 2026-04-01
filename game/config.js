2e50c7de3c35e11b3efc1d859392f4004655608a
Bogdan Popa <67425099+shimrut@users.noreply.github.com>
2026-04-01 08:54:47 +0300

    Add 'royalPlateau' to visibleTrackKeys
    Signed-off-by: Bogdan Popa <67425099+shimrut@users.noreply.github.com>

--- game/config.js
+++ game/config.js
@@ -1,7 +1,7 @@
 // --- Game Config ---
 export const CONFIG = {
     gridSize: 40, 
-    visibleTrackKeys: ['circuit', 'harborParkLoop', 'jadeSpiralCircuit', 'cedarRidgeCircuit', 'sakuraWeave'],
+    visibleTrackKeys: ['circuit', 'harborParkLoop', 'jadeSpiralCircuit', 'cedarRidgeCircuit', 'sakuraWeave', 'royalPlateau'],
     
     // Environment Colors
     offTrackColor: '#0f172a', // Deep Blue/Grey for runoff areas
