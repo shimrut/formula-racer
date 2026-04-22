5b1225ce03f744efcee00904d0ae214605cf0a07
Bogdan Popa <67425099+shimrut@users.noreply.github.com>
2026-04-22 08:52:58 +0300

    Add 'moebiusStrip' to visibleTrackKeys
    Signed-off-by: Bogdan Popa <67425099+shimrut@users.noreply.github.com>

--- game/config.js
+++ game/config.js
@@ -1,7 +1,7 @@
 // --- Game Config ---
 export const CONFIG = {
     gridSize: 40, 
-    visibleTrackKeys: ['circuit', 'blueSector', 'harborParkLoop', 'jadeSpiralCircuit', 'cedarRidgeCircuit', 'sakuraWeave', 'royalPlateau', 'twinRise'],
+    visibleTrackKeys: ['circuit', 'moebiusStrip', 'blueSector', 'harborParkLoop', 'jadeSpiralCircuit', 'cedarRidgeCircuit', 'sakuraWeave', 'royalPlateau', 'twinRise'],
     
     // Environment Colors
     offTrackColor: '#0f172a', // Deep Blue/Grey for runoff areas
