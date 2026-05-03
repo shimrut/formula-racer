491a9836f9e52535ca66608a768185d6e396f3ec
Bogdan Popa <67425099+shimrut@users.noreply.github.com>
2026-05-03 22:22:49 +0300

    Add 'kettleRun' to visibleTrackKeys
    Signed-off-by: Bogdan Popa <67425099+shimrut@users.noreply.github.com>

--- game/config.js
+++ game/config.js
@@ -1,7 +1,7 @@
 // --- Game Config ---
 export const CONFIG = {
     gridSize: 40, 
-    visibleTrackKeys: ['circuit', 'sunlitTemple', 'moebiusStrip', 'blueSector', 'harborParkLoop', 'jadeSpiralCircuit', 'cedarRidgeCircuit', 'sakuraWeave', 'royalPlateau', 'twinRise'],
+    visibleTrackKeys: ['circuit', 'sunlitTemple', 'moebiusStrip', 'blueSector', 'harborParkLoop', 'jadeSpiralCircuit', 'cedarRidgeCircuit', 'sakuraWeave', 'royalPlateau', 'twinRise', 'kettleRun'],
     
     // Environment Colors
     offTrackColor: '#0f172a', // Deep Blue/Grey for runoff areas
