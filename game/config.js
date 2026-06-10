d8cc1c54b9d7fd9c293260444d1b32d03ca70ba1
Bogdan Popa <67425099+shimrut@users.noreply.github.com>
2026-06-10 09:26:37 +0300

    Update visibleTrackKeys in game config
    Signed-off-by: Bogdan Popa <67425099+shimrut@users.noreply.github.com>

--- game/config.js
+++ game/config.js
@@ -1,7 +1,7 @@
 // --- Game Config ---
 export const CONFIG = {
     gridSize: 40, 
-    visibleTrackKeys: ['circuit', 'velvetWombat', 'kettleRun', 'alloyRing', 'mistwoodSerpent', 'carbonBend', 'sunlitTemple', 'moebiusStrip', 'blueSector', 'harborParkLoop', 'jadeSpiralCircuit', 'cedarRidgeCircuit', 'sakuraWeave', 'royalPlateau', 'twinRise'],
+    visibleTrackKeys: ['circuit', 'greenTroll', 'velvetWombat', 'kettleRun', 'alloyRing', 'mistwoodSerpent', 'carbonBend', 'sunlitTemple', 'moebiusStrip', 'blueSector', 'harborParkLoop', 'jadeSpiralCircuit', 'cedarRidgeCircuit', 'sakuraWeave', 'royalPlateau', 'twinRise'],
     
     // Environment Colors
     offTrackColor: '#0f172a', // Deep Blue/Grey for runoff areas
